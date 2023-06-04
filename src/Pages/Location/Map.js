b192b78ff4a4c276ea46548861c7885d9a707fa3
withsme <tmdal9953@gmail.com>
2023-06-04 18:20:14 +0900

    Feat 실시간 위치 정보 지도 표시

--- src/Pages/Location/Map.js
+++ src/Pages/Location/Map.js
@@ -1,33 +1,69 @@
-const { kakao } = window;
+/* eslint-disable no-lone-blocks */
+import React, { useEffect, useState } from 'react';
+import Mark from '../../Images/mark.png';
+import Bus from '../../Images/bus_location.png';
+ 
+const Map = ({routeId}) => {
+  const { naver } = window;
+  const [list, setList] = useState({ nodeDto: { latitude: 36.139, longitude: 128.396 } });
 
-function Map() {
-  const container = document.getElementById('myMap');
-  const options = {
-    center: new kakao.maps.LatLng(36.1425, 128.3945),
-    level: 5
-  };
-
-  // 지도를 생성합니다
-  const map = new kakao.maps.Map(container, options);
+  useEffect(()=> {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
   
-  // var imageSrc = "../../Images/white.png"; // 마커이미지의 주소입니다    
-  // var imageSize = new kakao.maps.Size(64, 69); // 마커이미지의 크기입니다
-  // var imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-        
-  // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-  // var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)
+    fetch(`http://119.56.230.204:506/api/routes/location?routeId=${routeId}`, requestOptions)
+    .then(response => response.json())
+    .then(result => setList(result))
+    .catch(error => console.log('error :: ', error))  
+  }, [])
 
-  // 마커가 표시될 위치입니다
-  var markerPosition  = new kakao.maps.LatLng(36.14256247287736, 128.39459855613836); 
-
-  // 마커를 생성합니다
-  var marker = new kakao.maps.Marker({
-    position: markerPosition, 
-    // image: markerImage // 마커이미지 설정 
+  useEffect(() => {
+    const location = new naver.maps.LatLng(36.13948, 128.39673); 
+    const map = new naver.maps.Map('map', {
+      center: location,
+      zoom: 14,
+    });
+  
+    {list.passingNodeList && list.passingNodeList.map((item) => {
+      const marker = new naver.maps.Marker({
+        map,
+        position: new naver.maps.LatLng(item.latitude, item.longitude),
+        icon: {
+          url: Mark,
+          size: new naver.maps.Size(50, 52),
+          origin: new naver.maps.Point(0, 0),
+          anchor: new naver.maps.Point(25, 26)
+        }
+      })
+    })}
+  
+    {list.realtimeNodeOrderList && list.realtimeNodeOrderList.map((item) => {
+      {list.passingNodeList && list.passingNodeList.map((items) => {
+        if (items.order == item) {
+          const busMarker = new naver.maps.Marker({
+            map,
+            position: new naver.maps.LatLng(items.latitude, items.longitude),
+            icon: {
+              url: Bus,
+              origin: new naver.maps.Point(0, 0),
+              anchor: new naver.maps.Point(25, 26)
+            }
+          })
+        }
+      })}
+    })}
   });
+  
+  console.log(list);
+  // console.log(routeId);
 
-  // 마커가 지도 위에 표시되도록 설정합니다
-  marker.setMap(map);
-}
-
+  return (
+    <div>
+      <div id="map" style={{ width:'100%', height:'65vh' }}></div>
+    </div>
+  );
+};
+ 
 export default Map;
